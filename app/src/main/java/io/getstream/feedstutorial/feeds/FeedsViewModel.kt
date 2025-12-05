0c3bc3e7f2ea64403c4dfed42f1813c9dd8ddace
Gian <47775302+gpunto@users.noreply.github.com>
2025-12-05 14:35:38 +0100

    Creating feeds

--- app/src/main/java/io/getstream/feedstutorial/feeds/FeedsViewModel.kt
+++ app/src/main/java/io/getstream/feedstutorial/feeds/FeedsViewModel.kt
@@ -3,9 +3,61 @@ package io.getstream.feedstutorial.feeds
 import android.app.Application
 import android.net.Uri
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.application
+import androidx.lifecycle.viewModelScope
+import io.getstream.feeds.android.client.api.FeedsClient
 import io.getstream.feeds.android.client.api.model.ActivityData
+import io.getstream.feeds.android.client.api.state.Feed
+import io.getstream.feedstutorial.ClientProvider
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class FeedsViewModel(application: Application) : AndroidViewModel(application) {
+    val state: StateFlow<State?> =
+        flow { emit(createState()) }
+            .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), null)
+
+    data class State(
+        val client: FeedsClient,
+        val userFeed: Feed,
+        val timelineFeed: Feed,
+    )
+
+    private suspend fun createState(): State {
+        val client = ClientProvider.get(application)
+        val state = State(
+            client = client,
+            userFeed = client.feed("user", client.user.id),
+            timelineFeed = client.feed("timeline", client.user.id),
+        )
+        loadFeeds(state)
+        return state
+    }
+
+    private suspend fun loadFeeds(state: State) {
+        state.userFeed.getOrCreate()
+        state.timelineFeed.getOrCreate()
+
+        viewModelScope.launch { followSelfIfNeeded(state) }
+    }
+
+    // By default, `timeline:user_id` doesn't follow `user:user_id` so the timeline doesn't
+    // show the user's own posts. For tutorial purposes we create the follow relationship
+    // here, but this logic is usually implemented in the backend.
+    private suspend fun followSelfIfNeeded(state: State) {
+        val followsSelf = state.timelineFeed.state.following.first()
+            .any { it.targetFeed.fid == state.userFeed.fid }
+        if (!followsSelf) {
+            state.timelineFeed.follow(
+                targetFid = state.userFeed.fid,
+                createNotificationActivity = false
+            )
+        }
+    }
 
     fun onPost(text: String, imageUri: Uri?) {
         // TODO: Implement as part of the tutorial
